199b2b29e61d70b1650a91ba6c06d68e78483068
Nam HQ <namhq.1989@gmail.com>
2024-02-01 10:51:42 +0700

    chore(server): add missing files

---
+++ pkg/sentryio/transaction.go
@@ -0,0 +1,17 @@
+package sentryio
+
+import (
+	"context"
+
+	"github.com/getsentry/sentry-go"
+)
+
+func NewTransaction(ctx context.Context, name string, data map[string]string) *sentry.Span {
+	t := sentry.StartTransaction(ctx, name)
+
+	// set data
+	for k, v := range data {
+		t.SetData(k, v)
+	}
+	return t
+}
