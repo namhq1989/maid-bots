1b34132c9224b1750ea77fbcab38d3d4fc0586aa
Nam Huynh <namhq1989@users.noreply.github.com>
2024-02-25 19:10:01 +0700

    feat(server): add api stats (#23)

--- platform/web/handler/monitor.go
+++ platform/web/handler/monitor.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"os"
+
 	"github.com/labstack/echo/v4"
 	modelresponse "github.com/namhq1989/maid-bots/internal/model/response"
 	"github.com/namhq1989/maid-bots/internal/service"
@@ -16,6 +18,7 @@ type Monitor struct{}
 // @tags Monitor
 // @summary List
 // @id monitor-list
+// @security ApiKeyAuth
 // @accept json
 // @produce json
 // @router /monitors/list [get]
@@ -53,5 +56,39 @@ func (Monitor) List(c echo.Context) error {
 
 	return response.R200(c, "", echo.Map{
 		"monitors": result,
+	})
+}
+
+// Stats godoc
+// @tags Monitor
+// @summary Stats
+// @id monitor-stats
+// @security ApiKeyAuth
+// @accept json
+// @produce json
+// @router /monitors/stats [get]
+func (Monitor) Stats(c echo.Context) error {
+	var (
+		ctx    = echocontext.GetContext(c)
+		userID = echocontext.GetUserID(c)
+		code   = c.Param("code")
+		s      = service.Monitor{}
+	)
+
+	// convert owner id
+	ownerID, err := mongodb.ObjectIDFromString(userID)
+	if err != nil {
+		return response.R400(c, err.Error(), echo.Map{})
+	}
+
+	result, err := s.StatsByCode(ctx, ownerID, code)
+	if err != nil {
+		return response.R400(c, err.Error(), echo.Map{})
+	}
+
+	defer func() { _ = os.Remove(result.ChartImageName) }()
+
+	return response.R200(c, "", echo.Map{
+		"stats": result,
 	})
 }
